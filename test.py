c3db54e097beaefb26ed9c73a896c5ca7ecd75e8
SamuelLopezF <samuellopez.f@gmail.com>
2023-02-16 20:04:57 -0500

    Adde blur & whitelisting

--- test.py
+++ test.py
@@ -1,42 +1,39 @@
 import cv2
 import pytesseract
+import re
 
 
-vid = cv2.VideoCapture("NARUTO.mp4")
+vid = cv2.VideoCapture(r"NARUTO.mp4")
 
 frameCount = 0
 while(True):
-    
+
     ret, frame = vid.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if frameCount % 30 == 0:
 
-    gray_gpu = cv2.UMat(gray)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray_gpu = gray_gpu.get()
+        gray_gpu = cv2.UMat(gray)
+        gray_gpu = gray_gpu.get()
 
-    # _, tresh = cv2.threshold(gray_gpu, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        blur = cv2.GaussianBlur(gray_gpu, (5,5), 0)
+        _, frame_thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    # tresh = tresh.get()
-
-
-    # img_rbg = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
-    if frameCount %30 == 0:
-        print(pytesseract.image_to_string(gray_gpu))
-
-    if not ret:
-        print("cannot recieve framerouni")
-        break
-
-    # cv2.imshow('frame', gray)
+        input = frame_thresh
+        result = pytesseract.image_to_string(input, config=r"--psm 3")
+        filtered = re.sub('[^a-zA-Z!.,\n ]', '', result )
+        print(filtered)
+        cv2.imshow('frame', input)
 
     frameCount += 1
+
+    if not ret:
+        print("cannot recieve framerouni or end of footage")
+        break
+    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
-
-    
-
-
 vid.release()
 cv2.destroyAllWindows()
